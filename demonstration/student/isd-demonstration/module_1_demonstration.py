d268378707e045d84f74a829c69c01f355ef618f
Allendale Nato <anato2@academic.rrc.ca>
2025-08-27 14:11:20 -0500

    feat: Answer module_1 _demonstration in isd demo.

--- demonstration/student/isd-demonstration/module_1_demonstration.py
+++ demonstration/student/isd-demonstration/module_1_demonstration.py
@@ -3,24 +3,30 @@ __version__ = "1.0.0"
 __credits__ = ""
 
 # 1. Import as necessary.
-
-
+from course.course import Course
+from department.department import Department
 
 def main():
     # 1. Create an instance of the Course class with valid inputs.
     #    Print the instance.
     #    If an exception occurs, print the exception instance.
-
-
-
+    try:
+        course = Course("ISD", Department.COMPUTER_SCIENCE, 6)
+        print(course)
+    except ValueError as e:
+        print(e)
 
     # 2. Create an instance of the Course class with invalid inputs.
     # Print the instance.
     # If an exception occurs, print the exception instance.
+    try:
+        invalid = Course("ISD", "Invalid Department", 6)
+        print(invalid)
+    except ValueError as e:
+        print(e)
 
 
 if __name__ == "__main__":
     main()
-    
-    
-    
+
+
