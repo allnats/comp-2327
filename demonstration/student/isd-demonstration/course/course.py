b495fe466d57b385e1015e55666c16db6c5c70e7
Allendale Nato <anato2@academic.rrc.ca>
2025-09-16 13:00:27 -0500

    feat: extend Course class to include capacity and current enrollment attributes

--- demonstration/student/isd-demonstration/course/course.py
+++ demonstration/student/isd-demonstration/course/course.py
@@ -1,9 +1,11 @@
 __author__ = "Allendale Nato"
 __version__ = "1.0.0"
 
+from abc import ABC, abstractmethod
 from department.department import Department
+from student.student import Student
 
-class Course:
+class Course(ABC):
     """Represents a course.
 
     Args:
@@ -14,8 +16,11 @@ class Course:
 
     """
 
-    def __init__(self, name: str, department: Department,
-                 credit_hours: int) -> None:
+    def __init__(self, name: str,
+                 department: Department,
+                 credit_hours: int,
+                 capacity: int,
+                 current_enrollment: int) -> None:
         """Initialize the Course class."""
         if len(name.strip()) <= 0:
             raise ValueError("Name connot be blank.")
@@ -26,10 +31,22 @@ class Course:
         if not isinstance(credit_hours, int):
             raise ValueError("Credit hours must be numeric.")
 
+        if not isinstance(capacity, int):
+            raise ValueError("Capacity must be numeric.")
+
+        if not isinstance(current_enrollment, int):
+            raise ValueError("Capacity must be numeric.")
+
+
         # Name mangling _ClassName.__name
+        # Private variables
         self.__name = name
         self.__department = department
         self.__credit_hours = credit_hours
+
+        # Protected variables
+        self._capacity = capacity
+        self._current_enrollment = current_enrollment
 
     @property
     def name(self) -> str:
